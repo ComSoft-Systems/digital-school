d4826f31bdd266d3bb4f1c114c54958f79177274
Hadia <hadiashahzadi@gmail.com>
2020-02-11 17:03:31 +0500

    hadia shahzadi commit 6 templates in development

---
+++ school/dependencies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.classes),
+    path('', views.schools),
+    path('', views.fee_category),
+    path('', views.section),
+    path('', views.family),
+    path('', views.session)
+]
---
+++ school/dependencies/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def classes(request):
+    return render(request, 'dependencies/classes.html')
+
+def schools(request):
+    return render(request, 'dependencies/schools.html')
+
+def fee_category(request):
+    return render(request, 'dependencies/fee_category.html')
+
+def section(request):
+    return render(request, 'dependencies/section.html')
+
+def family(request):
+    return render(request, 'dependencies/family.html')
+
+def session(request):
+    return render(request, 'dependencies/session.html')
