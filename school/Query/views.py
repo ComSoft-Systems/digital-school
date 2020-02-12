3539d9fabe9faae2124c35ba9b8d41990397f800
Nabeeha <nabeehashahzadi20@gmail.com>
2020-02-12 13:46:07 +0500

    Query App With structure

--- school/Query/views.py
+++ school/Query/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , HttpResponse
 
 def home(request):
     return render(request, 'query/entry_test.html')
