f13bdc7e0c48e14b4618ff68eea93e04207837b4
Nabeeha <nabeehashahzadi20@gmail.com>
2020-02-13 19:43:29 +0500

    pull from development into  nabeeha

--- school/Query/models.py
+++ school/Query/models.py
@@ -9,7 +9,6 @@ class Entry_data(models.Model):
     Address = models.CharField(max_length=30)
     last = models.ForeignKey(Class, on_delete= models.CASCADE)
     Previous_school = models.ForeignKey(School, on_delete= models.CASCADE)
-    Addmission_required = models.ForeignKey(Class, on_delete= models.CASCADE, related_name='required')
     Test_performed = models.BooleanField()
     Suggested_class = models.ForeignKey(Class, on_delete= models.CASCADE, related_name='suggested')
     test_teacher = models.CharField(max_length=30)
