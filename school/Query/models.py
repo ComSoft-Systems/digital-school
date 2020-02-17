433b37c859c0c6269b78887d3c739642aad87662
Nabeeha <nabeehashahzadi20@gmail.com>
2020-02-17 14:49:00 +0500

    nabeeha

--- school/Query/models.py
+++ school/Query/models.py
@@ -2,18 +2,34 @@ from django.db import models
 from django.utils import timezone
 from dependencies.models import Fee_Category , Class , School , Section , Family
 
+class PublishedManager(models.Manager):				
+    def	get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
+
 class Entry_data(models.Model):
     Query_code = models.IntegerField(primary_key=True, auto_created=True, unique=True)
     Name = models.CharField(max_length=30, verbose_name="Name")
     father_name = models.CharField(max_length=30, verbose_name="Father")
     Address = models.CharField(max_length=30)
-    last = models.ForeignKey(Class, on_delete= models.CASCADE)
-    Previous_school = models.ForeignKey(School, on_delete= models.CASCADE)
+    last = models.ForeignKey(Class, on_delete= models.CASCADE, default=1)
+    Previous_school = models.ForeignKey(School, on_delete= models.CASCADE, default=1)
+    Addmission_required = models.ForeignKey(Class, on_delete= models.CASCADE, related_name='required', default=1)
     Test_performed = models.BooleanField()
     Suggested_class = models.ForeignKey(Class, on_delete= models.CASCADE, related_name='suggested')
     test_teacher = models.CharField(max_length=30)
     date_of_test = models.DateField()
     Fee_type = models.ForeignKey(Fee_Category, on_delete= models.CASCADE)
     Contact = models.CharField(max_length=20)
+    objects	=models.Manager()
+    published =	PublishedManager()
+    def	get_absolute_url(self):								
+        return reverse('Query:Entry_data_detail',
+                    args=[self.publish.Name,
+                          self.publish.Father,
+                          self.publish.last,
+                          self.publish.Address
+                          ])
     def __str__(self):
         return self.Name
--- school/Query/urls.py
+++ school/Query/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .import views
 
+app_name = 'blog'
 
 urlpatterns = [
     path('', views.home),
     path('form/', views.form),
     path('list/', views.list),
-    path('detail/', views.detail)
+    path('detail/', views.detail),
+    path('e_list/', views.Entry_data_list, name='Entry_data_list'),
+    path('e_detail/', views.Entry_data_detail, name='Entry_data_detail')
 ]
