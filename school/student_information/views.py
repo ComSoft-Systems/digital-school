25e632ff98e061bc0412c7dacbfa99111c607642
TabishAliMughal <tabishalimughal@gmail.com>
2020-02-19 16:37:28 +0500

    information added

--- school/student_information/views.py
+++ school/student_information/views.py
@@ -15,10 +15,6 @@ def ManageGrCreateView(request):
         user_form = EntryForm(request.POST)
         if user_form.is_valid():
             form = user_form.save()
-            # for item in Gr:
-            #     OrderItem.objects.create( gr_number =['gr_number'] , query_code =['query_code'] , name =['name'] , picture =['picture'] , family_code =['family_code'] , fee_category_code =['fee_category_code'] , class_of_admission =['class_of_admission'] , session_of_admission =['session_of_admission'] , current_class =['current_class'] , current_session =['current_session'] , admission_date =['admission_date'] , last_school =['last_school'] , religion =['religion',] , date_of_birth =['date_of_birth'])
-            # cart.clear()
-            # order_created.delay(order.id)
             return render(request,'Student/Create/created.html')
     else:
         user_form = EntryForm()
