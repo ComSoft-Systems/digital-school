16a8c4a0d8bd5a72e26f5bf5ec7c2d65c852ffce
Nabeeha <nabeehashahzadi20@gmail.com>
2020-02-27 12:51:28 +0500

    change

--- school/Query/urls.py
+++ school/Query/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('home/', views.home),
     path('form/', views.form , name = 'entry_form'),
     path('list/', views.list),
     path('detail/', views.detail)
