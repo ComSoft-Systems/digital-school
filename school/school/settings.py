a7a2d541929e65901482d749c0763141f80875fd
Nabeeha <nabeehashahzadi20@gmail.com>
2020-02-28 13:49:26 +0500

    with edit and delete buttons

--- school/school/settings.py
+++ school/school/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'digi_school',
         'USER': 'comsoft',
         'PASSWORD': 'Admin@123',
-        'HOST': '127.0.0.1',
+        'HOST': '192.168.1.100',
         'PORT': '5432',
 
 
