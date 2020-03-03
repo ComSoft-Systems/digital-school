334ebac98f7da9f672d69593974ba159829937a1
TabishAliMughal <tabishalimughal@gmail.com>
2020-03-03 12:44:20 +0500

    authentication syatem added

--- school/school/settings.py
+++ school/school/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'student_information.apps.StudentInformationConfig',
     'dependencies',
     'main',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,7 @@ DATABASES = {
         'NAME': 'digi_school',
         'USER': 'comsoft',
         'PASSWORD': 'Admin@123',
-        'HOST': '192.168.1.111',
+        'HOST': '192.168.1.101',
         'PORT': '5432',
     }
 }
