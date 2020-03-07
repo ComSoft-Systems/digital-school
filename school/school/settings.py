8ebc42129aaf63d48ca592b4e2314afefdbcf1f0
Rizwan <rizwansarwar330@gmail.com>
2020-03-07 18:39:45 +0500

    home work Compelete

--- school/school/settings.py
+++ school/school/settings.py
@@ -87,7 +87,8 @@ DATABASES = {
         'NAME': 'digi_school',
         'USER': 'comsoft',
         'PASSWORD': 'Admin@123',
-        'HOST': '192.168.1.100',
+        # 'HOST': '192.168.1.101',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
