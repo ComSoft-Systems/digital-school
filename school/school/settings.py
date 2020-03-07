40beaa3895091745d273b464fc74996437660918
HadiaShahzadi <hadiashahzadi@gmail.com>
2020-03-07 11:32:41 +0500

    auto field increment created by hadia shahzadi

--- school/school/settings.py
+++ school/school/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'digi_school',
         'USER': 'comsoft',
         'PASSWORD': 'Admin@123',
-        'HOST': '192.168.1.100',
+        'HOST': '192.168.1.101',
         'PORT': '5432',
     }
 }
