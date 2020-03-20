bf2dc076e2a5366431161dd36ab53e588c0f8321
HadiaShahzadi <hadiashahzadi@gmail.com>
2020-03-20 14:48:24 +0500

    attendence added

--- school/attendence/forms.py
+++ school/attendence/forms.py
@@ -1,16 +1,15 @@
-# from django import forms
-# from django.forms import ModelForm
-# from .models import *
+from django import forms
+from django.forms import ModelForm
+from .models import *
 
-# class Attendence_Form(forms.ModelForm):
-    # def __init__(self, *args, **kwargs):
-    #     super(Attendence_Form, self).__init__(*args, **kwargs)
-    #     self.fields['checkBox'].widget.attrs.update({
-    #         'id': 'special',
-    #     })
-#     class Meta:
-#         model = SelectAttendence
-#         fields = {
-#             'classes', 
-#             'sections', 
-#         }
+class Attendence_Form(forms.ModelForm):
+    class Meta:
+        model = SaveAttendence
+        fields = [
+            'gr' ,
+            'family' ,
+            'classes' ,
+            'sections' ,
+            'attendence' ,
+            'date' ,
+        ]
--- school/attendence/models.py
+++ school/attendence/models.py
@@ -5,20 +5,10 @@ from dependencies.models import *
 
 # Create your models here. 
 class SaveAttendence(models.Model):
-    attendence_ID = models.AutoField(primary_key = True , unique = True)
-    attendence_date = models.DateField()
-    attendence_name = models.ForeignKey(Gr, on_delete = models.CASCADE)
-    attendence_father = models.ForeignKey(Family, on_delete = models.CASCADE)
-    attendence_classes = models.ForeignKey(Class, on_delete = models.CASCADE)
-    attendence_sections = models.ForeignKey(Section, on_delete = models.CASCADE)
-    attendence_checkBox = models.CharField(max_length = 20)
-
-
-class SelectAttendence(models.Model):
-    select_attendence_ID = models.AutoField(primary_key = True , unique = True)
-    name = models.ForeignKey(Gr, on_delete = models.CASCADE)
-    father = models.ForeignKey(Family, on_delete = models.CASCADE)
+    code = models.AutoField(primary_key = True)
+    gr = models.ForeignKey(Gr, on_delete = models.CASCADE)
+    family = models.ForeignKey(Family, on_delete = models.CASCADE)
     classes = models.ForeignKey(Class, on_delete = models.CASCADE)
     sections = models.ForeignKey(Section, on_delete = models.CASCADE)
-    checkBox = models.ForeignKey(SaveAttendence, on_delete = models.CASCADE)
-     
+    attendence = models.CharField(max_length = 20)
+    date = models.DateField()
